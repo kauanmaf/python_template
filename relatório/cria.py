dbe7f34e0ac44ca1812fdfe5a92317a16d9dae96
KAUAN MARIANI FERREIRA <B51058@fgv.edu.br>
2023-04-24 09:09:00 -0300

    adicionei o latex e corrigi alguns erros

--- relatório/cria.py
+++ relatório/cria.py
@@ -1,7 +1,12 @@
 class Relatório:
-    def __init__(self) -> None:
+    def __init__(self, title, author, date) -> None:
         self.cabeçalho=None
+        self.title = title
+        self.author = author
+        self.date = date
+        self.inicialize()
+
 
     def inicialize(self):
         with open('../dados/cabeçalho.tex') as f:
-            self.cabeçalho = f.head
+            self.cabeçalho = f.head()
