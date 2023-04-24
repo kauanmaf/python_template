7acc5eb285b21265c3126ec19f297758113ef9f5
KAUAN MARIANI FERREIRA <B51058@fgv.edu.br>
2023-04-24 09:01:19 -0300

    adicionei a pasta dados e o primeiro código

---
+++ relatório/cria.py
@@ -0,0 +1,7 @@
+class Relatório:
+    def __init__(self) -> None:
+        self.cabeçalho=None
+
+    def inicialize(self):
+        with open('../dados/cabeçalho.tex') as f:
+            self.cabeçalho
