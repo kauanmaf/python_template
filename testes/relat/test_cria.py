a1a042234210355d3dc4bdeb721127b5abff9dd9
KAUAN MARIANI FERREIRA <B51058@fgv.edu.br>
2023-04-26 08:33:35 -0300

    criei Teste.cria

---
+++ testes/relat/test_cria.py
@@ -0,0 +1,7 @@
+import unittest
+from relatorio.cria import Relatório
+
+class TestCria(unittest.TestCase):
+    def test_cria_realtorio(self)
+        R = Relatório('Meu Relatório', 'Eu mesmo')
+        self.assertEqual(R.titulo, 'Meu relatório')
